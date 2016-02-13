9e8c5c52b46d43f692d474566531f2186ac38c5b
Steven Lopez <slope029@ucr.edu>
2016-02-12 17:36:56 -0800

    adding files to repo

--- tests/commented_command.sh
+++ tests/commented_command.sh
@@ -5,4 +5,14 @@ echo ls -a && junk && ls # && ls
 
 echo junk && #dont output this
 
+echo #dont do any of this; || this || ls
 
+echo ls && #; test
+
+echo junk && ls && # echo here
+
+echo LS || LS || # || stuff
+
+echo "#" TEST THIS OUT
+
+echo #done with the test cases here
--- tests/exit.sh
+++ tests/exit.sh
@@ -6,3 +6,22 @@ echo &&exit
 echo junk || exit
 
 echo exit
+
+echo keep going?
+
+echo should be done exit
+
+echo I think that's why it's red;
+ 
+echo ls; && junk
+
+echo cout rungging sjlnj endl
+
+echo exit # typing for enought test cases
+
+echo # won't get here but exit
+
+echo exit(0)
+
+echo ccccccccc exit
+
