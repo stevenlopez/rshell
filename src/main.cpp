9ccd787d745512c096fca1bf5db148ea162ca3f8
Steven Lopez <slope029@ucr.edu>
2016-03-04 16:32:48 -0800

    commiting so tests can be added in

--- src/main.cpp
+++ src/main.cpp
@@ -14,12 +14,48 @@
 using namespace std;
 using namespace boost;
 
-int execute(char** args, bool &prev, bool &prev_prev, int con, int prev_con){ 
+
+
+void Print(vector<string> pts);
+
+void Build_Paren(vector<string> &P_str, int &P_sz, int &Paren_sz);
+//Par_prev_prev = Par previous prev or the previous connection
+// Par_prev = the next connection
+
+
+int execute(vector<string> s_t_e, bool &prev, bool &prev_prev, int con, int prev_con, bool Par_prev, bool Par_prev_prev, int Par_connn, int Par_connnn_prev, int paren_size_e, bool in_paren_e, int parseSize){ 
    //pass in the command and previous
+   //cout << "PAR_prev: " << Par_prev << endl;
+   //cout << "PAR_prev_prev: " << Par_prev_prev << endl;
+   //cout << "PAR_CONNECTION: " << Par_connnn_prev << endl;
+   //cout << "connection: " << con << endl;
+   char** args = new char*[128];                        //creates new array to store the characters
+    for(int i = 0; i < parseSize; i++){
+        char * temp = strdup(s_t_e.at(i).c_str());
+        args[i] = temp;  
+    }
+    args[parseSize] = '\0'; 
+   
+   
+   if(in_paren_e == true){
+   //cout << "IN PAREN executing: \n";
+    
+    if((Par_prev == false) && (con != 2) && (Par_connnn_prev != 2))                //may need to get rid of the last condition
+    {
+        //cout << "FAIL 1: \n";
+        return EXIT_FAILURE;   
+    }
+    if((Par_prev == true) && (Par_connnn_prev == 2)){        //might need just par_prev_prev
+        //cout << "FAIL 2: \n";
+        return EXIT_FAILURE;
+    }
+   }
     if((prev == false) && (con != 2) && (prev_con != 2)){                //check if the last one was false
+    //cout << "FAILED normal: \n";
         return EXIT_FAILURE;              //will/should only work for &&
     }
     if((prev == true) && (prev_con == 2)){
+        //cout << "FAILED normal: \n";
       return 0;//EXIT_FAILURE;
     }
         if (strcmp(args[0], "exit") == 0){    //if exit is the command 
@@ -53,19 +89,52 @@ int execute(char** args, bool &prev, bool &prev_prev, int con, int prev_con){
 return EXIT_SUCCESS;                         //else return 0 for true
 }
        	    
-void Parse(vector<string> &ParseString, int size, bool P_previous, bool P_previous_prev, int P_conn, int P_conn_prev){
+void Parse(vector<string> &ParseString, int size, bool P_previous, bool P_previous_prev, int P_conn, int P_conn_prev, bool Par_previous, bool Par_previous_prev, int Par_conn, int Par_conn_prev, int Par_conn_next, int &Paren_size, bool in_paren){
 if(size < 1){                                       //passing in nothing = return
     return;
 }
+cout << in_paren << endl;
+if((P_conn == 3) && (in_paren != true)){                        //////May need to change this back, erase in paren
+    cout << "RESETTING: \n";
+    Par_previous = true;
+    Par_previous_prev = false;
+}
+int paren_size = Paren_size;
+if(paren_size <= 0){
+    in_paren = false;
+    paren_size = 0;
+}
+
+if(in_paren == false){
+    //cout << "changing par connection: \n";
+    Par_conn_prev = P_conn; 
+}
 
 if(ParseString.at(0).at(0) == '('){
-cout << "paranthesis: " << endl;
+    //cout << "parenthesis: " << endl;
+    in_paren = true;
+    if(in_paren == true){
+        Build_Paren(ParseString, size, paren_size);
+    }    
 }
+//Print(ParseString);
+//cout << endl;
+//cout << "SIZE: " << paren_size << endl;
 
 int ParseSize;                                      // will need variables for later
 string connector;                                   //will hold the ;/&&/||
+
+int Par_con = Par_conn_next;
+int Par_con_next = Par_conn_next;
+
+int Par_con_prev = Par_conn_prev;
+    
 int P_con_prev = P_conn;
 int P_con;
+
+
+
+
 unsigned i = 0;                                     //basic counter
 vector<string> S_T_E;                               //empty vector to be filled in
 
@@ -106,19 +175,67 @@ string SP = ParseString.at(0);                       //if input is ;/&&/|| then 
               //cout << " in ;" << endl;
         }
 
+
+
+
+
+
+if(ParseSize > 126){                             // limit of 127 strings as one command
+        cout << "INPUT TOO LARGE" << endl;
+        return;
+    }
+/*    
 char** args = new char*[128];                        //creates new array to store the characters
     for(int i = 0; i < ParseSize; i++){
         char * temp = strdup(S_T_E.at(i).c_str());
         args[i] = temp;  
     }
+*/
+    cout << "Executing this string: \n";
+    Print(S_T_E);
+    cout << endl;
+    cout << "END STRING: \n";
+    //args[ParseSize] = '\0';                          // null terminating character at the end
     
-    if(ParseSize > 126){                             // limit of 127 strings as one command
-        cout << "INPUT TOO LARGE" << endl;
-        return;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //cout << "Par previous: " << Par_previous << endl;
+    //cout << "Par previous previous: " << Par_previous_prev << endl;
+    int temp;
+    //cout << S_T_E.at(0) << S_T_E.at(ParseSize - 1) << endl;
+    if((S_T_E.at(0) == "test") || ((S_T_E.at(0) == "[") && (S_T_E.at(ParseSize - 1) == "]"))){
+        cout << "Create test  case: \n";
     }
-    args[ParseSize] = '\0';                          // null terminating character at the end
+    //INERT MAGIC HERE
+    else{
+    temp = execute(S_T_E, P_previous, P_previous_prev, P_con, P_con_prev, Par_previous, Par_previous_prev, Par_con, Par_con_prev, paren_size, in_paren, ParseSize);              //checks whether it executed or not 
+    }
     
-    int temp = execute(args, P_previous, P_previous_prev, P_con, P_con_prev);              //checks whether it executed or not 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //bool Par_temp = P_previous_prev;                //might need to edit this
     P_previous_prev = P_previous;  
    
     if(temp == 0){                                  // sets out boolean P_previous
@@ -127,31 +244,82 @@ char** args = new char*[128];                        //creates new array to stor
     else{
         P_previous = false;
     }
+    //if(in_paren == true){
+    //    Par_previous = P_previous;
+    //}
+    //else{
+    //    Par_previous = Par_temp;                 //FIX HERE
+    //}
+    //Par_previous_prev
     
+    if(in_paren != true){
+        Par_con_next = P_con_prev;
+    }
+    
+    if(in_paren != true){
+    //cout << "changing par previous: \n ";
+        Par_previous = P_previous;
+    }
         if(connector == "0"){                   //should be empty but parse will handle it
             ParseString.erase(ParseString.begin(), ParseString.begin() + i);
+            if(in_paren == true){
+                paren_size = paren_size - i;
+                    if(paren_size <= 0){
+                        Par_con_prev = Par_con_next;
+                        Par_con_next = 0;
+                         Par_previous = P_previous;
+                    }
+            }
+            //cout << Par_con_prev << " " << Par_con << " " << P_con << endl;
             
             P_previous = true;
-            Parse(ParseString, ParseString.size(), P_previous, P_previous_prev, P_con, P_con_prev);
+            Parse(ParseString, ParseString.size(), P_previous, P_previous_prev, P_con, P_con_prev, Par_previous, Par_previous_prev, Par_con, Par_con_prev,  Par_con_next, paren_size, in_paren);
         }   
         if(connector == "&&"){                   //&& case(only one not set to true*******)
             ParseString.erase(ParseString.begin(), ParseString.begin() + i + 1);
+            if(in_paren == true){
+                paren_size = paren_size - i - 1;
+                    if(paren_size <= 0){
+                        Par_con_prev = Par_con_next;
+                        Par_con_next = 1;
+                        Par_previous = P_previous;
+                    }
+            }
+            //cout << Par_con_prev << " " << Par_con << " " << P_con << endl;
             
-            Parse(ParseString, ParseString.size(), P_previous, P_previous_prev, P_con, P_con_prev);
+            Parse(ParseString, ParseString.size(), P_previous, P_previous_prev, P_con, P_con_prev, Par_previous, Par_previous_prev, Par_con, Par_con_prev,  Par_con_next, paren_size, in_paren);
         }
         if(connector == "||"){                   //|| case
             ParseString.erase(ParseString.begin(), ParseString.begin() + i + 1);
+            if(in_paren == true){
+                paren_size = paren_size - i - 1;
+                    if(paren_size <= 0){
+                        Par_con_prev = Par_con_next;
+                        Par_con_next = 2;
+                        Par_previous = P_previous;
+                    }
+            }
+            //cout << Par_con_prev << " " << Par_con << " " << P_con << endl;
             
             if((P_previous_prev == true) && (P_con_prev == 2)){
             P_previous = true;
             }
-            Parse(ParseString, ParseString.size(), P_previous, P_previous_prev, P_con, P_con_prev);
+            Parse(ParseString, ParseString.size(), P_previous, P_previous_prev, P_con, P_con_prev, Par_previous, Par_previous_prev, Par_con, Par_con_prev, Par_con_next, paren_size, in_paren);
         }
         if(connector == ";"){                    //; case
             ParseString.erase(ParseString.begin(), ParseString.begin() + i + 1);
+            if(in_paren == true){
+                paren_size = paren_size - i - 1;
+                    if(paren_size <= 0){
+                        Par_con_prev = Par_con_next;
+                        Par_con_next = 3;
+                         Par_previous = P_previous;
+                    }
+            }
+            //cout << Par_con_prev << " " << Par_con << " " << P_con << endl;
             
             P_previous = true;
-            Parse(ParseString, ParseString.size(),  P_previous, P_previous_prev, P_con, P_con_prev);
+            Parse(ParseString, ParseString.size(),  P_previous, P_previous_prev, P_con, P_con_prev, Par_previous, Par_previous_prev, Par_con, Par_con_prev, Par_con_next, paren_size, in_paren);
         }
         return;
 }
@@ -189,8 +357,8 @@ void Print(vector<string> pts){                  //function that prints string v
 
 bool Prec_checker(vector<string> prec){
 stack<char>  S;
-	for(int j = 0; j < prec.size(); ++j){
-    for(int i = 0; i < prec.at(j).size(); ++i){
+	for(unsigned j = 0; j < prec.size(); ++j){
+    for(unsigned i = 0; i < prec.at(j).size(); ++i){
 		if(prec.at(j).at(i) == '(')
 			S.push(prec.at(j).at(i));
       
@@ -205,12 +373,52 @@ stack<char>  S;
 	return S.empty() ? true:false;
 }
 
-//////////////////////IGNORE THIS CODE, JUST DELETES THE COMMENT AND PRINTS////////////////////////////////////
+void erase_end_paren(string &s, bool &end_par){
+    for(unsigned i = 0; i < s.size(); ++i){
+        if(s.at(i) == ')'){
+            s.erase(s.begin()+i, s.end());
+            end_par = true;
+        }
+    }
+}
+
+
+
+void Build_Paren(vector<string> &P_str, int &P_sz, int &Paren_sz){
+string temp = P_str.at(0);
+temp.erase(temp.begin());
+
+P_str.at(0) = temp;
+vector<string> change_parse;
+bool build_end_par = false;
+for(int i = 0; i < P_sz; ++i)
+{
+    if(build_end_par == false){
+        string find_end = P_str.at(i);
+        erase_end_paren(find_end, build_end_par);
+        change_parse.push_back(find_end);
+        Paren_sz = i + 1;
+    }
+    else{
+        string find_finish = P_str.at(i);
+        change_parse.push_back(find_finish);
+    }
+}
+P_str = change_parse;
+}
+
+//////////////////////IGNORE THIS CODE, JUST DELETES THE COMMENT, CHECKS PARANTHESIS BALANCE, AND PRINTS////////////////////////////////////
 
 int main(){
 
 bool MainPrevious = true;   //allows us to run our first command
 bool MainPrevious_prev = false;
+
+bool Par_MainPrevious = true;   //allows us to run our first command
+bool Par_MainPrevious_prev = false;
+
+bool in_paren = false;
+int Begin_size = 0;
 vector<string> cmd;         //vector to hold our string
 string str;                 //will simply be our input string
 
@@ -234,7 +442,7 @@ while(1){
     cmd.erase(cmd.begin(), cmd.end());
     }
     int size = cmd.size();                                   //gets updated size(if changed)
-    Parse(cmd, size, MainPrevious, MainPrevious_prev, 0, 0);                          //starts parsing commands
+    Parse(cmd, size, MainPrevious, MainPrevious_prev, 0, 0,  Par_MainPrevious, Par_MainPrevious_prev, 0, 0, 0, Begin_size, in_paren);                          //starts parsing commands
     }
 
 return 0;
