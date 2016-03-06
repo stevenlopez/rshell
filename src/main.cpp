f13029238b5731e47c1291bccfa732589ea141f7
Nancy Li <nli006@hammer.cs.ucr.edu>
2016-03-05 16:18:54 -0800

    test command part 1

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <cmath>
@@ -87,6 +88,87 @@ void Print(vector<string> pts){                  //function that prints string v
       cout << *it << " ";                      //not really needed(the print function)
       }
 }
+
+             ////////// assn 3 code  //////////////
+int checkFlag(vector<string> str){
+    int flag = 0;
+    if(str.at(0) == "-e"){
+        flag = 1;
+    }
+    else if(str.at(0) == "-f"){
+        flag = 2;
+    }
+    else if(str.at(0) == "-d"){
+        flag = 3;
+    }
+    return flag;
+}
+
+bool ifExists(vector<string> &ParseString, int flag){
+    unsigned i = 0;
+    while(i < ParseString.size()){
+        if(flag == 1 || flag == 0){}
+        else if(flag == 2){}
+        else if( flag == 3){}
+    }
+    return false;
+}
+
+
+
+
+void testFunctionality(vector<string> ParsedString, unsigned index){
+    int flag = checkFlag(ParsedString);
+    unsigned end = ParsedString.size();
+    if(index > 0){
+       end = index;
+    }
+    // check what's in vector
+    for(unsigned i = 0; i < end; ++i){
+        cout << ParsedString.at(i) << ' ';
+    }
+
+    char *path;
+    if(flag == 0){
+        path = strdup((ParsedString.at(0)).c_str());
+    }
+    else{
+        path = strdup((ParsedString.at(1)).c_str());
+    }    
+
+    struct stat s;
+    if(flag == 0 || flag == 1){
+        if(stat(path, &s) == 0){
+            cout << "(TRUE)" << endl;
+        }
+        else{
+            cout << "(FALSE)" << endl;
+        }
+    }
+    else if(flag == 2){
+        if(s.st_mode & S_IFREG){
+            cout << "(TRUE)" << endl;
+        }
+        else{
+            cout << "(FALSE)" << endl;
+        }
+    }
+    else if(flag == 3){
+        if(s.st_mode & S_IFDIR){
+            cout << "(TRUE)" << endl;
+        }
+        else{
+            cout << "(FALSE)" << endl;
+        } 
+    }
+    else{
+        cout << "Error" << endl;
+        exit(-1);
+    }
+}
+////////////////////////////////////////////////////////////////////
+
+
        	    
 
 void Parse(vector<string> &ParseString, int size, bool P_previous){
@@ -94,20 +176,31 @@ if(size < 1){                                       //passing in nothing = retur
     return;
 }
 
-
 int ParseSize;                                      // will need variables for later
 string connector;                                   //will hold the ;/&&/||
 unsigned i = 0;                                     //basic counter
+unsigned j = 0;                                     //counter for matching bracket for test functionality
+unsigned h = 0;
+bool test_cmd = false;
+bool matching = false;                               //does matching bracket exist
+//bool bracket = false;
 vector<string> S_T_E;                               //empty vector to be filled in
+int temp = -1;
 
 while((i < ParseString.size())
     && (ParseString.at(i) != ";")                   //creates the string 
     && (ParseString.at(i) != "||")                  //that will execute
     && (ParseString.at(i) != "&&")){                //stops when connector/empty
+    //assn 3 code    
+    if(ParseString.at(0) == "test" || ParseString.at(0) == "["){
+            test_cmd = true;
+        }
+        
         S_T_E.push_back(ParseString.at(i));
         ++i;
         ParseSize = i;
     }
+    // assn3 code
 
 string SP = ParseString.at(0);                       //if input is ;/&&/|| then error
     if((SP == ";") || (SP == "||") || (SP == "&&")){
@@ -115,11 +208,10 @@ string SP = ParseString.at(0);                       //if input is ;/&&/|| then 
         cout << "bash: syntax error near unexpected token '" <<  SP << "'" << endl;
         return;
     }
-    
-    
-    
+   
+
         if(i == ((ParseString.size()))){            //checks the last character
-           connector = "0";                        // 0 connector
+            connector = "0";                        // 0 connector
            //cout << " in 0" << endl;
         }                                           //sets connnector
             
@@ -131,12 +223,59 @@ string SP = ParseString.at(0);                       //if input is ;/&&/|| then 
               connector = "&&";
               //cout << " in &&" << endl;
         }
-        else if(ParseString.at(i) == "||"){         //|| connector
+        else if(ParseString.at(i) == "||"){        //|| connector
               connector = "||"; 
               //cout << " in ||" << endl;
         }
+        
+        // assignment 3 code. check for test commands
+        
+        if(test_cmd == true){
+            cout << "entered" << endl;
+                while(j < ParseString.size()){ 
+                    if(ParseString.at(j) == "test"){
+                        S_T_E.erase(S_T_E.begin() + j);
+                        --ParseSize;
+                        testFunctionality(S_T_E, 0);
+                        cout << checkFlag(S_T_E) << endl;
+                        break;
+                    }
+                    else if(ParseString.at(j) == "["){
+                        while(h < ParseString.size()){
+                            if(ParseString.at(h) == "]"){
+                                S_T_E.erase(S_T_E.begin() + j);
+                                --ParseSize;
+                                testFunctionality(S_T_E, 0);
+                                cout << checkFlag(S_T_E) << endl;
+                                matching = true;
+                                break;
+                            }
+                            ++h;
+                        }
+                        
+                        if(matching == false){
+                            cout << "Error: no matching bracket" << endl;
+                            exit(-1);
+                        }
+                        
+                    }
 
-char** args = new char*[128];                        //creates new array to store the characters
+                    ++j;
+                }
+        }
+        
+        //end test command if-statement
+
+
+    char** args = new char*[128];                        //creates new array to store the characters
+    
+    if(test_cmd == true || matching == true){           //assn3 code
+        cout << "line 284" << endl;
+        goto skipexec;    
+    } // assn 3 code
+    
+    cout << "line 288" << endl;
+
     for(int i = 0; i < ParseSize; i++){
         char * temp = strdup(S_T_E.at(i).c_str());
         args[i] = temp;  
@@ -146,9 +285,10 @@ char** args = new char*[128];                        //creates new array to stor
         cout << "INPUT TOO LARGE" << endl;
         return;
     }
-    args[ParseSize] = '\0';                          // null terminating character at the end
-    
-    int temp = Test(args, P_previous);              //checks whether it executed or not      
+    args[ParseSize] = '\0';      // null terminating character at the end
+   
+
+    temp = Test(args, P_previous);              //checks whether it executed or not      
     if(temp == 0){                                  // sets out boolean P_previous
         P_previous = true;
     }
@@ -156,7 +296,7 @@ char** args = new char*[128];                        //creates new array to stor
         P_previous = false;
     }
     
-    
+skipexec:
         if(connector == "0"){                   //should be empty but parse will handle it
             ParseString.erase(ParseString.begin(), ParseString.begin() + i);
             
@@ -182,8 +322,24 @@ char** args = new char*[128];                        //creates new array to stor
             Parse(ParseString, ParseString.size(),  P_previous);
             
         }
+        //assn 3 code
+        if(test_cmd == true || matching == true){
+            if(matching == true){
+                ParseString.erase(ParseString.begin(), ParseString.begin() +  + 2);
+                P_previous = true;
+                Parse(ParseString, ParseString.size(), P_previous);
+            }
+            else{
+                ParseString.erase(ParseString.begin(), ParseString.begin() + i + 1);
+                P_previous = true;
+                Parse(ParseString, ParseString.size(), P_previous);
+            }
+        }
+        // assn 3 code
+        
         return;
 }
+
 
 
 int main(){
